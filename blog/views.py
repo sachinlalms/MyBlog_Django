d6164b87877707d41f9c61f1815a61dcc1f7afd8
sachinlalms <sachinlalms95@gmail.com>
2020-09-07 22:20:40 +0530

    listV/DeleteV/updateV

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 from django.http import HttpResponse
 # Create your views here.
@@ -21,9 +29,40 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+        model = Post
+        success_url = '/'
+
+        def test_func(self):
+            post = self.get_object()
+            if self.request.user == post.author:
+                return True
+            return False
 
 
 
 def about(request):
-    return render(request,'blog/about.html',{'title': 'D_about'})
+    return render(request,'blog/about.html',{'title': 'about'})
     # return HttpResponse('<h1>Blog Abouts</h1>')
