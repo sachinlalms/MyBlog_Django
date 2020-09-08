971e18d71bdd652c6d693b5005e8adfa11b606ae
sachinlalms <sachinlalms95@gmail.com>
2020-09-08 14:24:21 +0530

    last

--- myblog/settings.py
+++ myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sachinlalms95@gmail.com'
 EMAIL_HOST_PASSWORD = 'zahxdymxuajybyvk'
+
+django_heroku.settings(locals())
